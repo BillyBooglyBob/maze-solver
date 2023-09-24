72ae6326f024493e37771045c18c4f0395dafe06
BillyBooglyBob <123722533+BillyBooglyBob@users.noreply.github.com>
2023-09-25 01:03:12 +1000

    Created File Loader
    Can load specific map into a 2D array with proper exception handling and testing.

--- src/io/FileLoader.java
+++ src/io/FileLoader.java
@@ -3,10 +3,7 @@ package io;
 import exceptions.MazeMalformedException;
 import exceptions.MazeSizeMissmatchException;
 
-import java.io.BufferedReader;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.IOException;
+import java.io.*;
 
 /**
  * Class for loading the map of a maze file into a 2D array
@@ -19,7 +16,10 @@ public class FileLoader implements FileInterface {
      * @param filename The path to the maze file to be loaded.
      * @return 2D array of the maze
      * @throws MazeMalformedException If the maze data is not correctly formatted.
+     *                                For example, the first line is not the dimension.
+     *                                Or the line doesn't start and end with a wall.
      * @throws MazeSizeMissmatchException If the maze dimensions do not match the provided size.
+     *                                    For example, the width of the line exceeds the dimension.
      * @throws IllegalArgumentException For other validation errors.
      * @throws FileNotFoundException If the maze file is not found.
      */
@@ -34,36 +34,119 @@ public class FileLoader implements FileInterface {
         convert each line into a charArray[]
         convert each corresponding row into charArray[]
 
+
          */
+
+        // check if file exists
+        File file = new File(filename);
+        if (!file.exists()) {
+            // The file does not exist, so throw FileNotFoundException
+            throw new FileNotFoundException("File not found.");
+        }
+
         try {
             FileReader fileReader = new FileReader(filename);
             BufferedReader bufferedReader = new BufferedReader(fileReader);
             String line;
 
-            // read the dimension
-            // check it is the dimension (only 2 odd integers)
+            // read the first line which should contain the dimensions
             int rows;
             int columns;
+            // used to keep track of the first and last line of the map
+            int rowCounter = 0;
+
             line = bufferedReader.readLine();
             String[] dimensions = line.split(" ");
+
+            // minus 1 as counting starts from 0
             rows = Integer.parseInt(dimensions[0]);
             columns = Integer.parseInt(dimensions[1]);
 
-            if (rows % 2 == 0 && columns % 2 == 0) {
+            // verify the dimension is valid (both are odd)
+            if (rows % 2 == 0 && columns % 2 == 0
+                || rows < 0 || columns < 0) {
                 throw new IllegalArgumentException();
             }
 
-            while ((line = bufferedReader.readLine()) != null) {
+            char[][] map;
+            map = new char[rows][columns];
 
+            // subtract value to ensure rows and rowsCounter both start at 0
+            rows--;
+
+            // read the map
+            while ((line = bufferedReader.readLine()) != null) {
+                int lengthOfLine = line.length();
+                // check width of map
+                if (lengthOfLine != columns) {
+                    throw new MazeSizeMissmatchException();
+                }
+
+                // check border of the map
+                // first and last line must all be walls
+                // start and end of any other lines must both be walls
+                // rows - 1 as rowCounter starts at 0 and rows at 1
+                if (rowCounter == 0 || rowCounter == rows) {
+                    for (int i = 0; i < lengthOfLine; i++) {
+                        char a = line.charAt(i);
+                        if (a != '#') {
+                            throw new MazeMalformedException();
+                        }
+                    }
+                } else {
+                    char firstChar = line.charAt(0);
+                    char lastChar = line.charAt(lengthOfLine - 1);
+                    if (firstChar != '#' || lastChar != '#') {
+                        throw new MazeMalformedException();
+                    }
+                    else {
+                        for (int i = 0; i < lengthOfLine; i++) {
+                            char a = line.charAt(i);
+                            if (a != '#' && a != ' ' && a != '.' && a != 'S' && a != 'E') {
+                                throw new MazeMalformedException();
+                            }
+                        }
+                    }
+                }
+
+                // check rows of the map matches with the rows of the dimension
+                // accounts for when there are fewer rows or more rows
+                if (rowCounter > rows) {
+                    throw new MazeSizeMissmatchException();
+                }
+
+                // check each character is correct
+
+
+                // add line to the array
+                map[rowCounter] = line.toCharArray();
+
+                rowCounter++;
+            }
+
+            // map has fewer rows than the specified dimension
+            if (rowCounter < rows) {
+                throw new MazeSizeMissmatchException();
             }
 
 
-            return new char[0][];
+            return map;
         } catch (IOException e) {
-            System.out.println("IO error occured");;
+            System.out.println("IO error occurred.");
         }
 
 
         return new char[0][];
+    }
+
+    public static void main(String[] args) throws FileNotFoundException, MazeSizeMissmatchException, MazeMalformedException {
+        FileLoader a = new FileLoader();
+        char[][] b = a.load("maze-solver\\test\\mazes\\TooFewRows.txt");
+        for (char[] chars : b) {
+            for (char aChar : chars) {
+                System.out.print(aChar + " ");
+            }
+            System.out.println(); // Move to the next line after each row
+        }
     }
 }
--- test/io/FileLoaderTest.java
+++ test/io/FileLoaderTest.java
@@ -1,22 +1,108 @@
 package io;
 
+import exceptions.MazeMalformedException;
+import exceptions.MazeSizeMissmatchException;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+
+import java.io.FileNotFoundException;
+import java.util.Arrays;
 
 import static org.junit.Assert.*;
 
 public class FileLoaderTest {
 
+    private FileLoader fileLoader;
+
     @Before
-    public void setUp() throws Exception {
+    public void setUp() {
+        fileLoader = new FileLoader();
     }
 
     @After
-    public void tearDown() throws Exception {
+    public void tearDown() {
+        fileLoader = null;
     }
 
     @Test
-    public void load() {
+    public void loadTestSmallMap() throws FileNotFoundException, MazeSizeMissmatchException, MazeMalformedException {
+        char[][] actualMap = fileLoader.load("maze-solver\\src\\mazes\\Small.txt");
+        char[][] correctMap = {
+                {'#', '#', '#', '#', '#', '#', '#'},
+                {'#', 'S', '#', ' ', ' ', ' ', '#'},
+                {'#', ' ', '#', '#', '#', ' ', '#'},
+                {'#', ' ', '#', ' ', ' ', ' ', '#'},
+                {'#', ' ', '#', ' ', '#', ' ', '#'},
+                {'#', ' ', ' ', ' ', '#', 'E', '#'},
+                {'#', '#', '#', '#', '#', '#', '#'}
+        };
+        assertArrayEquals(correctMap, actualMap);
+    }
+
+//    @Test
+//    public void loadTestMediumMap() throws FileNotFoundException, MazeSizeMissmatchException, MazeMalformedException {
+//        char[][] actualMap = fileLoader.load("maze-solver\\src\\mazes\\Medium.txt");
+//        char[][] correctMap = {
+//                {'#', '#', '#', '#', '#', '#', '#'},
+//                {'#', 'S', '#', ' ', ' ', ' ', '#'},
+//                {'#', ' ', '#', '#', '#', ' ', '#'},
+//                {'#', ' ', '#', ' ', ' ', ' ', '#'},
+//                {'#', ' ', '#', ' ', '#', ' ', '#'},
+//                {'#', ' ', ' ', ' ', '#', 'E', '#'},
+//                {'#', '#', '#', '#', '#', '#', '#'}
+//        };
+//        assertArrayEquals(correctMap, actualMap);
+//    }
+//
+//    @Test
+//    public void loadTestLargeMap() throws FileNotFoundException, MazeSizeMissmatchException, MazeMalformedException {
+//        char[][] actualMap = fileLoader.load("maze-solver\\src\\mazes\\Large.txt");
+//        char[][] correctMap = {
+//                {'#', '#', '#', '#', '#', '#', '#'},
+//                {'#', 'S', '#', ' ', ' ', ' ', '#'},
+//                {'#', ' ', '#', '#', '#', ' ', '#'},
+//                {'#', ' ', '#', ' ', ' ', ' ', '#'},
+//                {'#', ' ', '#', ' ', '#', ' ', '#'},
+//                {'#', ' ', ' ', ' ', '#', 'E', '#'},
+//                {'#', '#', '#', '#', '#', '#', '#'}
+//        };
+//        assertArrayEquals(correctMap, actualMap);
+//    }
+
+    @Test (expected = IllegalArgumentException.class)
+    public void loadTestWrongDimension()
+            throws FileNotFoundException, MazeSizeMissmatchException, MazeMalformedException {
+        fileLoader.load("maze-solver\\test\\mazes\\WrongDimension.txt");
+    }
+
+    @Test (expected = IllegalArgumentException.class)
+    public void loadTestNoDimension()
+            throws FileNotFoundException, MazeSizeMissmatchException, MazeMalformedException {
+        fileLoader.load("maze-solver\\test\\mazes\\NoDimension.txt");
+    }
+
+    @Test (expected = MazeMalformedException.class)
+    public void loadTestMapWrongCharacter()
+            throws FileNotFoundException, MazeSizeMissmatchException, MazeMalformedException {
+        fileLoader.load("maze-solver\\test\\mazes\\WrongCharacter.txt");
+    }
+
+    @Test (expected = MazeSizeMissmatchException.class)
+    public void loadTestTooManyRows()
+            throws FileNotFoundException, MazeSizeMissmatchException, MazeMalformedException {
+        fileLoader.load("maze-solver\\test\\mazes\\TooManyRows.txt");
+    }
+
+    @Test (expected = MazeSizeMissmatchException.class)
+    public void loadTestTooFewRows()
+            throws FileNotFoundException, MazeSizeMissmatchException, MazeMalformedException {
+        fileLoader.load("maze-solver\\test\\mazes\\TooFewRows.txt");
+    }
+
+    @Test (expected = MazeSizeMissmatchException.class)
+    public void loadTestWrongWidth()
+            throws FileNotFoundException, MazeSizeMissmatchException, MazeMalformedException {
+        fileLoader.load("maze-solver\\test\\mazes\\WrongWidth.txt");
     }
 }
